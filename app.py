df1c4f16d77f0cfb5e59ebfabc0faf9b06c414cf
Prathamesh J. Pawar <prathmeshpawar212002@gmail.com>
2024-11-27 12:15:51 +0530

    Deploy Change 1

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ dataFrame = pd.read_csv("./data/Cardetails.csv")
 app = Flask(__name__)
 
 
-form_data = {}
 
 @app.route('/', methods=['POST','GET'])
 def base():
@@ -27,18 +26,18 @@ def base():
     car_company_names = dataFrame["name"].apply(nf.get_carBrandNames).unique().tolist()
     car_company_names.sort()
     # Fetch Form Inputted Data
-    if request.method == "POST":
-        name = request.form.get("carcompany")
-        fuel = request.form.get("fueltype")
-        seller_type = request.form.get("sellertype")
-        transmission = request.form.get("transmissiontype")
-        year = request.form.get("manufacturedyear")
-        owner = request.form.get("carowner")
-        km_driven = request.form.get("kmsdriven")
-        engine = request.form.get("carenginecc")
-        mileage = request.form.get("carmileage")
-        seats = request.form.get("noofseats")
-        SELLING_PRICE = 0
+    # if request.method == "POST":
+    #     name = request.form.get("carcompany")
+    #     fuel = request.form.get("fueltype")
+    #     seller_type = request.form.get("sellertype")
+    #     transmission = request.form.get("transmissiontype")
+    #     year = request.form.get("manufacturedyear")
+    #     owner = request.form.get("carowner")
+    #     km_driven = request.form.get("kmsdriven")
+    #     engine = request.form.get("carenginecc")
+    #     mileage = request.form.get("carmileage")
+    #     seats = request.form.get("noofseats")
+    #     SELLING_PRICE = 0
     # DataFrameMaking
     # InputDataDF = pd.DataFrame([[name, year, SELLING_PRICE, km_driven, fuel, seller_type,transmission, owner, mileage, engine, seats]],columns=['name', 'year', 'selling_price', 'km_driven', 'fuel', 'seller_type','transmission', 'owner', 'mileage', 'engine', 'seats'])
 
@@ -55,11 +54,8 @@ def base():
             "mileage": request.form.get("carmileage"),
             "seats": request.form.get("noofseats"),
         }
-        form_data = fromDataDict
     else:
         fromDataDict = {}
-
-    form_data = fromDataDict
 
     test_columns = [
         "year",
@@ -129,32 +125,24 @@ def base():
                 "seats": [fromDataDict["seats"]],
             }
         )
-
         categorical_columns = ["name", "fuel", "seller_type", "transmission", "owner"]
-
         ohe = OneHotEncoder(sparse_output=False)
         encodedOHEArr = ohe.fit_transform(input_data[categorical_columns])
         encodedOHEDf = pd.DataFrame(
             encodedOHEArr, columns=ohe.get_feature_names_out(categorical_columns)
         )
-
-        print(encodedOHEDf)
-
+        # print(encodedOHEDf)
         # Add missing columns and set their values to 0
         for col in test_columns:
             if col not in encodedOHEDf.columns:
                 encodedOHEDf[col] = 0
 
         # Ensure the column order matches the test data
-
         encodedOHEDf = encodedOHEDf[test_columns]
-
         encodedOHEDf.drop(
             columns=["year", "km_driven", "mileage", "engine", "seats"], inplace=True
         )
-
         # print(encodedOHEDf)
-
         nonEncodedData = input_data = pd.DataFrame(
             {
                 "year": [fromDataDict["year"]],
@@ -164,9 +152,7 @@ def base():
                 "seats": [fromDataDict["seats"]],
             }
         )
-
-        print("Print Here")
-
+        # print("Print Here")
         # Scaling Data from formula var get from notebook like mean, std of historical Data
         records = [
             float(fromDataDict["year"]),
@@ -175,7 +161,6 @@ def base():
             float(fromDataDict["engine"]),
             float(fromDataDict["seats"]),
         ]
-
         means = [
             float(2013.61114),
             float(73398.3377),
@@ -190,16 +175,11 @@ def base():
             float(493.432463),
             float(0.983731806),
         ]
-        # Example inputted form data (ensure the order matches training data columns)
-        # year, km_driven, engine, mileage, seats
-        # print(records)
-        # Scale the form data
+        
         scaled_form_data = [
             (value - mean) / std for value, mean, std in zip(records, means, stds)
         ]
-
-        print("Scaled Form Data:", scaled_form_data)
-
+        # print("Scaled Form Data:", scaled_form_data)
         inputScaledDataFrame = pd.DataFrame(
             {
                 "year": [scaled_form_data[0]],
@@ -209,32 +189,23 @@ def base():
                 "seats": [scaled_form_data[4]],
             }
         )
-
         test_input_Data_for_model = pd.concat([inputScaledDataFrame, encodedOHEDf], axis=1)
         print(test_input_Data_for_model)
 
         if test_input_Data_for_model.columns.tolist() == test_columns:
             print("SIMILLAR COLOUMS")
-
         #################################################################################
         FinalPredictedOutput = model.predict(test_input_Data_for_model)
         FinalPredictedOutput = FinalPredictedOutput[0]
         print("Predicted Value : ", FinalPredictedOutput)
-    #################################################################################
+        #################################################################################
 
-    # return render_template('./base.html', **fromDataDict, car_company_names=car_company_names,InputDataDF=InputDataDF,FinalPredictedOutput=FinalPredictedOutput)
 
     return render_template(
         "./base.html",
         car_company_names=car_company_names,
         FinalPredictedOutput=FinalPredictedOutput,
     )
-    return
-
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
-
-# name year selling_price km_driven fuel seller_type    transmission owner mileage engine seats
-
-# BMW  500   0             500      Diesel Trustmark Dealer Manual     500    500     500   500
